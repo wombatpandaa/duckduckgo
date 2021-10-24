edc760933322195f5bf77eb2687e871060555414
Chad <chadjpeterson21@gmail.com>
2021-10-23 23:29:40 -0400

    Create ddgsearch.py

---
+++ ddgsearch.py
@@ -0,0 +1,4 @@
+# query ddg api for "presidents of the united states"
+# test that each president is listed in the response by last name using pytest
+# use github repository as scc
+# use travis cl to continuously test as changes are pushed
